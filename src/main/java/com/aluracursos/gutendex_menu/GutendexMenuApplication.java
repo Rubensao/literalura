0f27d50ff61818cef4782d5b9a14b40a8287232f
Rubensao <ruben.sanhueza14@gmail.com>
2024-07-07 15:21:20 -0400

    Initial commit

---
+++ src/main/java/com/aluracursos/gutendex_menu/GutendexMenuApplication.java
@@ -0,0 +1,27 @@
+package com.aluracursos.gutendex_menu;
+
+import com.aluracursos.gutendex_menu.principal.Principal;
+import com.aluracursos.gutendex_menu.repository.AutorRepository;
+import com.aluracursos.gutendex_menu.repository.LibroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GutendexMenuApplication implements CommandLineRunner {
+
+	@Autowired
+	private LibroRepository libroRepository;
+	@Autowired
+	private AutorRepository autorRepository;
+	public static void main(String[] args) {
+		SpringApplication.run(GutendexMenuApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal(libroRepository, autorRepository);
+		principal.ejecutarPrograma();
+	}
+}
